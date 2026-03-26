d8cd11fe3bfe50b1c67e3532fe12c10e613fdccf
Logan Leger <logan@loganleger.com>
2026-03-25 21:51:53 -0700

    feat: switch elixir lsp to expert

---
+++ config/nvim/lua/plugins/elixir.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        elixirls = {
+          enabled = false,
+        },
+        expert = {},
+      },
+    },
+  },
+}
